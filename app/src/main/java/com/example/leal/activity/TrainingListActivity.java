8d381142d6d9b50f0b52e9cec8f1d61cc30b44e7
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-10 15:31:19 -0300

    create item_exercise.xml and activity_exercise_list.xml

--- app/src/main/java/com/example/leal/activity/TrainingListActivity.java
+++ app/src/main/java/com/example/leal/activity/TrainingListActivity.java
@@ -1,40 +1,36 @@
 package com.example.leal.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import com.example.leal.R;
-import com.example.leal.adapter.TrainingListAdapter;
 
 public class TrainingListActivity extends AppCompatActivity {
-    private RecyclerView trainingRecyclerView;
-    private Button trainingButton;
+    private RecyclerView trainingListRecyclerView;
+    private Button trainingListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_training);
+        setContentView(R.layout.activity_training_list);
         findViewsById();
-       // setupRecyclerView(trainingListAdapter);
+      //  setupRecyclerView(trainingListAdapter);
     }
 
 //    private void setupRecyclerView(TrainingListAdapter trainingListAdapter) {
-//        trainingRecyclerView.setAdapter(trainingListAdapter);
+//        trainingListRecyclerView.setAdapter(trainingListAdapter);
 //        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
 //                LinearLayoutManager.VERTICAL, false);
-//        trainingRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+//        trainingListRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
 //,
-//        trainingRecyclerView.setLayoutManager(layoutManager);
+//        trainingListRecyclerView.setLayoutManager(layoutManager);
 //    }
 
     private void findViewsById() {
-        trainingRecyclerView = findViewById(R.id.trainingRecyclerView);
-        trainingButton = findViewById(R.id.trainingButton);
+        trainingListRecyclerView = findViewById(R.id.trainingListRecyclerView);
+        trainingListButton = findViewById(R.id.trainingListButton);
     }
 }
