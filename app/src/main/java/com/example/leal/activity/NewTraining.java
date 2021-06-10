98107fc0e7007ce22b2ddec42e284b04ba55fa08
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-10 16:50:15 -0300

    create new_training.xml

---
+++ app/src/main/java/com/example/leal/activity/NewTraining.java
@@ -0,0 +1,16 @@
+package com.example.leal.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.leal.R;
+
+public class NewTraining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_training);
+    }
+}
